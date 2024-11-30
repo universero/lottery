1a5202c1c72893af692188220ca382a647379634
lllzhmio <13999806+lllzhmio@user.noreply.gitee.com>
2024-11-30 23:16:18 +0800

    更改右上角图标，配置抽奖信息

--- server/config.js
+++ server/config.js
@@ -17,49 +17,49 @@ const prizes = [
     type: 1,
     count: 2,
     text: "特等奖",
-    title: "神秘大礼",
+    title: "宁波人就用宁波银行——手机",
     img: "../img/secrit.jpg"
   },
   {
     type: 2,
-    count: 5,
+    count: 3,
     text: "一等奖",
-    title: "Mac Pro",
+    title: "德国米技厨房烹饪四件套",
     img: "../img/mbp.jpg"
   },
   {
     type: 3,
-    count: 6,
-    text: "二等奖",
-    title: "华为 Mate30",
+    count: 10,
+    text: "特别艺术奖",
+    title: "艺术无价情谊更高价——手绘艺术瓷瓶",
     img: "../img/huawei.png"
   },
   {
     type: 4,
-    count: 7,
-    text: "三等奖",
-    title: "Ipad Mini5",
+    count: 10,
+    text: "二等奖",
+    title: "手持吸尘器",
     img: "../img/ipad.jpg"
   },
   {
     type: 5,
-    count: 8,
-    text: "四等奖",
-    title: "大疆无人机",
+    count: 20,
+    text: "二等奖",
+    title: "10年陈皮",
     img: "../img/spark.jpg"
   },
   {
     type: 6,
-    count: 8,
-    text: "五等奖",
-    title: "Kindle",
+    count: 10,
+    text: "三等奖",
+    title: "创意手提音响",
     img: "../img/kindle.jpg"
   },
   {
     type: 7,
-    count: 11,
-    text: "六等奖",
-    title: "漫步者蓝牙耳机",
+    count: 20,
+    text: "三等奖",
+    title: "贝缔雅护手霜",
     img: "../img/edifier.jpg"
   }
 ];
@@ -67,12 +67,12 @@ const prizes = [
 /**
  * 一次抽取的奖品个数与prizes对应
  */
-const EACH_COUNT = [1, 1, 5, 6, 7, 8, 9, 10];
+const EACH_COUNT = [2,3,10,10,20,10,20];
 
 /**
  * 卡片公司名称标识
  */
-const COMPANY = "MoShang";
+const COMPANY = "北仑";
 
 module.exports = {
   prizes,
